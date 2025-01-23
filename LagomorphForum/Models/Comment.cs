5ef38ae09efd0f367b617bf89f53801112137857
domhnail <coledanodonnell@hotmail.com>
2025-01-23 15:53:58 -0400

    Add project files.

---
+++ LagomorphForum/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace LagomorphForum.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public int DiscussionId { get; set; }
+        public Discussion? Discussion { get; set; }
+    }
+}
---
+++ LagomorphForum/Models/Discussion.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+namespace LagomorphForum.Models
+{
+    public class Discussion
+    {
+        public int DiscussionId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ImageFilename { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public List<Comment>? Comment { get; set; }
+    }
+}
